7067b38b64fc1b2ad06b680f839aa24dda09aaa6
peterlee <peterlee0127@gmail.com>
2020-08-16 14:20:13 +0800

    Change config to file, request to axios

--- cloudflare.js
+++ cloudflare.js
@@ -1,9 +1,10 @@
 'use strict';
 const getIP = require('external-ip')();
-const request = require('request');
-const domain = 'xxxxxx.com'
-const email = 'xxxxxxx@gmail.com';
-const key = 'xxxxxxxxxxxxxx';
+const axios = require('axios');
+const config = require('./config.json');
+const domain = config.domain;
+const email = config.email;
+const key = config.key;
 
 const api = 'https://api.cloudflare.com/client/v4/';
 const headers = {
@@ -14,20 +15,25 @@ const headers = {
 
 function getZoneInfo() {
     let options = {
-	url:api+'zones',
-	name:domain,
-	status:'active',
- 	headers:headers
-    }
-    request.get(options,function(error,response,body){
-	let zones = JSON.parse(body).result;
-	let zone = [];
-	for(let i=0;i<zones.length;i++)	 {
-		zone.push(zones[i].id);
+		url: api+'zones',
+		name: domain,
+		status:'active',
+		headers:headers,
+		method: 'get'
 	}
-	getDNSRecord(zone);
+	axios(options).then(function (response) {
+		let body = response.data;
+		let zones = body.result;
+		let zone = [];
+		for(let i=0;i<zones.length;i++)	 {
+			zone.push(zones[i].id);
+		}
+		getDNSRecord(zone);
     	console.log(zone);
-    });
+	});
+    // request.get(options,function(error,response,body){
+	
+    // });
 
 }
 
@@ -35,19 +41,24 @@ function getDNSRecord(zones){
     for(let i=0;i<zones.length;i++){
     let zone = zones[i]
     let options = {
-	url:api+'zones/'+zone+"/dns_records",
-	headers:headers
-    }
-    request.get(options,function(error,response,body){
-	let json = JSON.parse(body).result;
-   	let ids = [];
-	for(let j=0;j<json.length;j++){
-		if(json[j].type=='A'){
-			ids.push(json[j]);
-		}
+		url:api+'zones/'+zone+"/dns_records",
+		headers:headers,
+		method: 'get'
 	}
-    	updateRecord(ids);
-    });
+	axios(options).then(function (response) {
+			let body = response.data;
+			let json = body.result;
+			let ids = [];
+			for(let j=0;j<json.length;j++){
+				if(json[j].type=='A'){
+					ids.push(json[j]);
+				}
+			}
+			 updateRecord(ids);
+		});
+    // request.get(options,function(error,response,body){
+
+    // });
 
   }
 }
@@ -66,21 +77,33 @@ getIP((err, ip) => {
 
 function updateRecord(records) {
  records.forEach(record => {
-   let body = {
-	"name":record.name,
-	"type":record.type,
-	"content":IP
-   }
-   body = JSON.stringify(body);
-   let options = {
-	url:api+'zones/'+record.zone_id+"/dns_records/"+record.id,
-	body:body,
-	headers:headers
-    }
-   request.put(options,function(error,response,body){
+   	let body = {
+		"name": record.name,
+		"type": record.type,
+		"content": IP,
+		"proxied": true
+	}
+  	body = JSON.stringify(body);
+   	let options = {
+		url:api+'zones/'+record.zone_id+"/dns_records/"+record.id,
+		data:body,
+		headers:headers,
+		method: 'put'
+	}
+	axios(options
+	) .then(function (response) {
+		let result = response.data.result;
+		console.dir({
+			'name': result.name,
+			'type': result.type,
+			'content': result.content,
+			'proxied': proxied
+		});
+	});
+//    request.put(options,function(error,response,body){
 
-   	console.dir(body,{depth:null});
-   });
+//    	console.dir(body,{depth:null});
+//    });
 
  });
 }
